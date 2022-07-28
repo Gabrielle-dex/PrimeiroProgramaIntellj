7db67183ad01d0b6c429254f651e03fedb62f6ff
Gabrielle <gabriellebetto@gmail.com>
2022-07-28 13:55:11 -0300

    https://github.com/Gabrielle-dex/PrimeiroProgramaIntellj.git

---
+++ src/PP_intellj.java
@@ -0,0 +1,11 @@
+import model.Gato;
+
+public class PP_intellj {
+    public static void main(String[] args){
+        Gato gato = new Gato();
+        System.out.println(gato);
+   /*     int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a+b)); */
+    }
+}
